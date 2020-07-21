cb9367e54bca03c1ce00746181794140562ab206
James Bowman <jamesb@excamera.com>
2020-07-20 20:06:49 -0700

    ECC codes

--- hdmi.v
+++ hdmi.v
@@ -3,33 +3,49 @@
 /* verilator lint_off DECLFILENAME */
 /* verilator lint_off UNUSED */
 
-module TMDS_encoderA(
-	input wire clk,
-	input wire [7:0] VD,  // video data (red, green or blue)
-	input wire [1:0] CD,  // control data
-	input wire VDE,  // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
-	output reg [9:0] TMDS = 0
+module tmds(
+  input wire clk,
+  input wire [7:0] VD,  // video data (red, green or blue)
+  input wire [1:0] CD,  // control data
+  input wire VDE,  // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
+  output reg [9:0] TMDS = 0
 );
 
-wire [3:0] Nb1s = {3'b000, VD[0]} + {3'b000, VD[1]} + {3'b000, VD[2]} + {3'b000, VD[3]} + {3'b000, VD[4]} + {3'b000, VD[5]} + {3'b000, VD[6]} + {3'b000, VD[7]};
-wire XNOR = (Nb1s>4'd4) || (Nb1s==4'd4 && VD[0]==1'b0);
-reg [8:0] q_m;
-always @(posedge clk) q_m <= {~XNOR, q_m[6:0] ^ VD[7:1] ^ {7{XNOR}}, VD[0]};
+  wire [3:0] Nb1s =
+    {3'b000, VD[0]} +
+    {3'b000, VD[1]} +
+    {3'b000, VD[2]} +
+    {3'b000, VD[3]} +
+    {3'b000, VD[4]} +
+    {3'b000, VD[5]} +
+    {3'b000, VD[6]} +
+    {3'b000, VD[7]};
+  wire XNOR = (Nb1s > 4'd4) || (Nb1s == 4'd4 && VD[0] == 1'b0);
+  reg [8:0] q_m;
+  always @(posedge clk) q_m <= {~XNOR, q_m[6:0] ^ VD[7:1] ^ {7{XNOR}}, VD[0]};
 
-reg [3:0] balance_acc = 0;
-wire [3:0] balance = {3'b000, q_m[0]} + {3'b000, q_m[1]} + {3'b000, q_m[2]} + {3'b000, q_m[3]} + {3'b000, q_m[4]} + {3'b000, q_m[5]} + {3'b000, q_m[6]} + {3'b000, q_m[7]} - 4'd4;
-wire balance_sign_eq = (balance[3] == balance_acc[3]);
-wire invert_q_m = (balance==0 || balance_acc==0) ? ~q_m[8] : balance_sign_eq;
-wire [3:0] balance_acc_inc = balance - {3'b000, ({q_m[8] ^ ~balance_sign_eq} & ~(balance==0 || balance_acc==0))};
-wire [3:0] balance_acc_new = invert_q_m ? balance_acc-balance_acc_inc : balance_acc+balance_acc_inc;
-wire [9:0] TMDS_data = {invert_q_m, q_m[8], q_m[7:0] ^ {8{invert_q_m}}};
-wire [9:0] TMDS_code = CD[1] ? (CD[0] ? 10'b1010101011 : 10'b0101010100) : (CD[0] ? 10'b0010101011 : 10'b1101010100);
+  reg [3:0] balance_acc = 0;
+  wire [3:0] balance =
+    {3'b000, q_m[0]} +
+    {3'b000, q_m[1]} +
+    {3'b000, q_m[2]} +
+    {3'b000, q_m[3]} +
+    {3'b000, q_m[4]} +
+    {3'b000, q_m[5]} +
+    {3'b000, q_m[6]} +
+    {3'b000, q_m[7]} - 4'd4;
+  wire balance_sign_eq = (balance[3] == balance_acc[3]);
+  wire invert_q_m = (balance==0 || balance_acc==0) ? ~q_m[8] : balance_sign_eq;
+  wire [3:0] balance_acc_inc = balance - {3'b000, ({q_m[8] ^ ~balance_sign_eq} & ~(balance==0 || balance_acc==0))};
+  wire [3:0] balance_acc_new = invert_q_m ? balance_acc-balance_acc_inc : balance_acc+balance_acc_inc;
+  wire [9:0] TMDS_data = {invert_q_m, q_m[8], q_m[7:0] ^ {8{invert_q_m}}};
+  wire [9:0] TMDS_code = CD[1] ? (CD[0] ? 10'b1010101011 : 10'b0101010100) : (CD[0] ? 10'b0010101011 : 10'b1101010100);
 
-always @(posedge clk) TMDS <= VDE ? TMDS_data : TMDS_code;
-always @(posedge clk) balance_acc <= VDE ? balance_acc_new : 4'h0;
+  always @(posedge clk) TMDS <= VDE ? TMDS_data : TMDS_code;
+  always @(posedge clk) balance_acc <= VDE ? balance_acc_new : 4'h0;
 endmodule
 
-module TERC4_encoder(
+module terc4(
     input wire [3:0] i,
     output reg [9:0] o
 );
@@ -56,23 +72,35 @@ always @*
 
 endmodule
 
-
-////////////////////////////////////////////////////////////////////////
-module HDMI_encoder_1(
+module video_data(
   input  wire [26:0] dd1,
   input  wire clk,
   output wire [29:0] d);
+
   wire [7:0] pR;
   wire [7:0] pG;
   wire [7:0] pB;
   wire DE, HSYNC, VSYNC;
   assign {pR, pG, pB, DE, HSYNC, VSYNC} = dd1;
   wire [9:0] TMDS_red, TMDS_green, TMDS_blue;
-  TMDS_encoderA encode_R(.clk(clk), .VD(pR), .CD(2'b00)          ,.VDE(DE), .TMDS(TMDS_red));
-  TMDS_encoderA encode_G(.clk(clk), .VD(pG), .CD(2'b00)          ,.VDE(DE), .TMDS(TMDS_green));
-  TMDS_encoderA encode_B(.clk(clk), .VD(pB), .CD({VSYNC,HSYNC})  ,.VDE(DE), .TMDS(TMDS_blue));
+  tmds encode_R(.clk(clk), .VD(pR), .CD(2'b00)          ,.VDE(DE), .TMDS(TMDS_red));
+  tmds encode_G(.clk(clk), .VD(pG), .CD(2'b00)          ,.VDE(DE), .TMDS(TMDS_green));
+  tmds encode_B(.clk(clk), .VD(pB), .CD({VSYNC,HSYNC})  ,.VDE(DE), .TMDS(TMDS_blue));
   assign d = {TMDS_red, TMDS_green, TMDS_blue};
 endmodule
+
+// Simplifications:
+//
+// Plenty of room between the HSYNC pulse and start of video, so data island does not overlap HSYNC
+// Always insert 3 data packets
+//     0 audio sample
+//     1 audio sample
+//     On a 45-line cycle:
+//        00: Audio Clock Regeneration Packet
+//        82: AVI InfoFrame
+//        83: Source Product Descriptor InfoFrame
+//        84: Audio InfoFrame
+//
 
 module hdmi(
   input  wire clk,
@@ -108,11 +136,18 @@ module hdmi(
       hblank <= hblank + 1;
   end
 
+  reg [23:0] pkt_hdr;
+  reg [7:0] hecc;
+
+  reg [55:0] pkt_bch;
+  reg [7:0] pecc;
+
   wire video_guards   = ~DE & prepipe[8][2];
   wire video_preamble = ~DE & ~video_guards & prepipe[0][2];
   wire data_preamble  = ~DE & ~HSYNC & (hblank < 11'd8);
   wire data_guard     = ((11'd8 <= hblank) & (hblank < 11'd10)) | ((11'd42 <= hblank) & (hblank < 11'd44));
   wire data_island    = (11'd10 <= hblank) & (hblank < 11'd42);
+  wire [4:0] dib = hblank[4:0] - 5'd10;
 
   wire [9:0] dp0 = VSYNC ? 10'b0101010100 : 10'b1101010100;
   wire [9:0] dg0 = VSYNC ? 10'b0101100011 : 10'b1010001110;
@@ -124,11 +159,19 @@ module hdmi(
   wire [29:0] o4 = {20'b_0100110011_0100110011, dg0};         // data guard
   wire [29:0] o5;                                             // TERC
 
-  TERC4_encoder _terc0 (.i({2'b10, VSYNC, HSYNC}), .o(o5[9:0]));
-  TERC4_encoder _terc1 (.i(4'd0), .o(o5[19:10]));
-  TERC4_encoder _terc2 (.i(4'd0), .o(o5[29:20]));
+  wire bh = &dib[4:3] ? hecc[0] : pkt_hdr[0];
+  wire [7:0] next_hecc = {1'b0, hecc[7:1]} ^ (((hecc[0] ^ bh)) ? 8'b10000011 : 8'd0);
 
-  HDMI_encoder_1 _e (.clk(clk), .dd1(prepipe[9]), .d(o0)); // has 1 clk latency, so use [9]
+  wire bp0 = &dib[4:2] ? pecc[0] : pkt_bch[0];
+  wire bp1 = &dib[4:2] ? pecc[1] : pkt_bch[1];
+  wire [7:0] pec0      = {1'b0, pecc[7:1]} ^ (((pecc[0] ^ bp0)) ? 8'b10000011 : 8'd0);
+  wire [7:0] next_pecc = {1'b0, pec0[7:1]} ^ (((pec0[0] ^ bp1)) ? 8'b10000011 : 8'd0);
+
+  terc4 _terc0 (.i({1'b1, bh, VSYNC, HSYNC}), .o(o5[9:0]));
+  terc4 _terc1 (.i({3'b000, bp0}), .o(o5[19:10]));
+  terc4 _terc2 (.i({3'b000, bp1}), .o(o5[29:20]));
+
+  video_data _e (.clk(clk), .dd1(prepipe[9]), .d(o0)); // has 1 clk latency, so use [9]
 
   always @*
     if (data_island)
@@ -144,7 +187,24 @@ module hdmi(
     else
       d = o0;
 
+  always @(posedge clk) begin
+    if (dib == 5'd31) begin
+      hecc <= 8'h00;
+      pkt_hdr <= 24'h0d0282;
+      pecc <= 8'h00;
+      pkt_bch <= 56'h00_04_00_08_00_63;
+    end else begin
+      pkt_hdr <= {1'b0, pkt_hdr[23:1]};
+      pkt_bch <= {2'b0, pkt_bch[55:2]};
+    end
+
+    hecc <= next_hecc;
+    pecc <= next_pecc;
+    if (VSYNC) $display("%d: %x %x", dib, pkt_bch, pkt_hdr);
+  end
+
 endmodule
 
 /* verilator lint_on UNUSED */
 /* verilator lint_on DECLFILENAME */
+
