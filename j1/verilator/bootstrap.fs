537a134751187816cce5618659d4c164054ab9bf
James Bowman <jamesb@excamera.com>
2020-11-27 18:02:18 -0800

    image builder

---
+++ j1/verilator/bootstrap.fs
@@ -0,0 +1,8 @@
+#include ../swapforth.fs
+#flash ../build/swapforth.hex
+#include ../swapforth/bringup.fs
+#flash ../build/bringup.hex
+#include ../swapforth/asteroids.fs
+#flash ../build/asteroids.hex
+#bye
+
