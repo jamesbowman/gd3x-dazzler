efab973b08117cd4fc84a97d3eecbf7c9e9d37ce
James Bowman <James Bowman>
2020-11-29 09:24:04 -0800

    test case runner

--- j1/swapforth/textmode.fs
+++ j1/swapforth/textmode.fs
@@ -6,7 +6,37 @@ CSPI
 : >gd   >spid ;
 : w>gd  0 >gd ;
 
-: getc 'a' ;
+defer getc
 
 #include ../../textmode/_textmode.fs
 #include ../../textmode/textmode2.fs
+
+: terminal
+    begin
+        getc
+        case
+        10 of down1 endof
+        13 of 0 cx ! endof
+        27 of csi endof
+        dup plain
+        endcase
+    again
+    ;
+
+2variable playback
+: setbuf playback 2! ;
+:noname
+    playback 2@
+    dup 0= if quit then
+    over c@ -rot
+    1 /string
+    setbuf
+; is getc
+
+: x
+    0 mux0
+    0 playstream
+    stream
+    ;
+
+: y s" brilliant" setbuf terminal ;
